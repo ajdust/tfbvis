07158e86db13524403ec2ba66ba27249852dde39
Aaron B Johnson <johnsoaab@gmail.com>
2019-11-19 21:35:27 -0800

    Add percent-based simple colored bar to RPS column

--- docs/main.js
+++ docs/main.js
@@ -31,7 +31,8 @@ var columnDefs = [
             },
             {
                 headerName: "RPS total", field: "rps.requests_per_sec",
-                filter: 'agNumberColumnFilter', sort: 'desc'
+                filter: 'agNumberColumnFilter', sort: 'desc',
+                cellRenderer: 'percentBarCellRenderer', width: 150
             },
             {
                 headerName: "RPS max per thread", field: "rps.thread_rps_max",
@@ -128,60 +129,128 @@ var columnDefs = [
     }
 ];
 
-var gridOptions = {
-    defaultColDef: {
-        sortable: true,
-        resizable: true,
-        width: 100,
-        cellStyle: { textAlign: "right" }
-    },
-    columnDefs: columnDefs,
+// cell renderer class
+function PercentBarCellRenderer() { }
+
+// init method gets the details of the cell to be rendere
+PercentBarCellRenderer.prototype.init = function (params) {
+    this.eGui = percentCellRenderer(params);
 };
 
-gridOptions.getRowStyle = function (params) {
-    return { "border-bottom": "1px solid " + params.data.meta.color }
-}
+PercentBarCellRenderer.prototype.getGui = function () {
+    return this.eGui;
+};
 
-function loadTable() {
-    var testtype = document.getElementById("testtype").value;
-    var key = "tfb_" + testtype;
+var percentCellRenderer = function (params) {
+    console.log(params);
+    var value = params.value;
+    var percent = 100 * value / window.TFB_GRID.minMaxes.maxRps;
 
-    if (window[key]) {
-        gridOptions.api.setRowData(window[key]);
-        return;
+    var eDivPercentBarWrapper = document.createElement('div');
+    eDivPercentBarWrapper.className = 'div-percent-bar-wrapper';
+    var eDivPercentBar = document.createElement('div');
+
+    eDivPercentBarWrapper.appendChild(eDivPercentBar);
+    eDivPercentBar.className = "div-percent-bar";
+    eDivPercentBar.style.width = percent + "%";
+
+    // eDivPercentBar.style.backgroundColor = data.meta.color || "#cccccc"
+
+    if (percent < 20) {
+        eDivPercentBar.style.backgroundColor = "#f55d51";
+    } else if (percent < 60) {
+        eDivPercentBar.style.backgroundColor = "#ffb300";
+    } else {
+        eDivPercentBar.style.backgroundColor = "#82d249";
     }
 
-    // lookup from TechEmpower's tfb-lookup.js
-    function attachMeta(fetchedData) {
-        for (var i = 0; i < fetchedData.length; i++) {
-            var fw = fetchedData[i];
-            fw.meta = {};
-            var meta = window.lookup.tests(fw.name);
-            if (meta) {
-                var attr = meta.attributes;
-                fw.meta.language = attr.language.name;
-                fw.meta.platform = attr.platform.name;
-                fw.meta.webserver = attr.webserver.name;
-                fw.meta.classification = attr.classification.name;
-                fw.meta.database = attr.database.name;
-                fw.meta.orm = attr.orm.name;
-                fw.meta.framework = attr.framework.name;
-                fw.meta.color = attr.language.opaque;
+    var eValue = document.createElement("div");
+    eValue.className = "div-percent-value";
+    eValue.innerHTML = value + " (" + percent + "%)";
+
+    var eOuterDiv = document.createElement("div");
+    eOuterDiv.className = "div-outer-div";
+    eOuterDiv.appendChild(eDivPercentBarWrapper);
+    eOuterDiv.appendChild(eValue);
+
+    return eOuterDiv;
+};
+
+window.TFB_GRID = {
+    gridOptions: {
+        defaultColDef: {
+            sortable: true,
+            resizable: true,
+            width: 100,
+            cellStyle: { textAlign: "right" }
+        },
+        columnDefs: columnDefs,
+        components: {
+            'percentBarCellRenderer': PercentBarCellRenderer
+        }
+        // getRowStyle: function (params) {
+        //     return { "border-bottom": "1px solid " + params.data.meta.color }
+        // }
+    },
+
+    loadTable: function () {
+        var testtype = document.getElementById("testtype").value;
+        var key = "data" + testtype;
+
+        var cached = TFB_GRID[key];
+        if (cached) {
+            TFB_GRID.minMaxes = calculateMinMaxes(cached);
+            TFB_GRID.gridOptions.api.setRowData(cached);
+            console.log('refreshhing');
+            TFB_GRID.gridOptions.api.refreshCells();
+            return;
+        }
+
+        // lookup from TechEmpower's tfb-lookup.js
+        function attachMeta(fetchedData) {
+            for (var i = 0; i < fetchedData.length; i++) {
+                var fw = fetchedData[i];
+                fw.meta = {};
+                var meta = window.lookup.tests(fw.name);
+                if (meta) {
+                    var attr = meta.attributes;
+                    fw.meta.language = attr.language.name;
+                    fw.meta.platform = attr.platform.name;
+                    fw.meta.webserver = attr.webserver.name;
+                    fw.meta.classification = attr.classification.name;
+                    fw.meta.database = attr.database.name;
+                    fw.meta.orm = attr.orm.name;
+                    fw.meta.framework = attr.framework.name;
+                    fw.meta.color = attr.language.opaque;
+                }
             }
         }
-    }
 
-    fetch(testtype + ".json")
-        .then(response => response.json())
-        .then(fetchedData => {
-            attachMeta(fetchedData);
-            window[key] = fetchedData;
-            loadTable();
-        });
-}
+        function calculateMinMaxes(fetchedData) {
+            var maxRps = 0, minLat90 = Infinity, maxMem = 0;
+            for (var i = 0; i < fetchedData.length; i++) {
+                var fw = fetchedData[i];
+                if (fw.rps.requests_per_sec > maxRps) maxRps = fw.rps.requests_per_sec;
+                if (fw.memory.max > maxMem) maxMem = fw.memory.max;
+                if (fw.latency.lat90 < minLat90) minLat90 = fw.latency.lat90;
+            }
+
+            return { maxRps: maxRps, minLat90: minLat90, maxMem: maxMem };
+        }
+
+        fetch(testtype + ".json")
+            .then(response => response.json())
+            .then(fetchedData => {
+                attachMeta(fetchedData);
+                TFB_GRID[key] = fetchedData;
+                TFB_GRID.minMaxes = calculateMinMaxes(fetchedData);
+                TFB_GRID.loadTable();
+            });
+    }
+};
 
 document.addEventListener('DOMContentLoaded', function () {
     var gridDiv = document.querySelector('#myGrid');
-    var grid = new agGrid.Grid(gridDiv, gridOptions);
-    loadTable();
+    new agGrid.Grid(gridDiv, TFB_GRID.gridOptions);
+    TFB_GRID.loadTable();
 });
